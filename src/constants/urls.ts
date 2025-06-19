0837117e5bd8844c5fa116d9e7c12e2febec930c
Stefan <stefischer@mail.com>
2025-06-19 16:34:35 -0400

    Update urls constants

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,1 +1,3 @@
 export const GITHUB_REPO = "https://github.com/RevenantEverest/themelab";
+export const LINKEDIN = "https://linkedin.com/in/Stefischer";
+export const PERSONAL_SITE = "https://stefischer.com";
