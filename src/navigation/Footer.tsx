06318f9cdffbcf1b76fa40ab183d22fde4c797a3
Stefan <stefischer@mail.com>
2025-06-19 16:34:58 -0400

    Modernize footer styling

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -1,37 +1,66 @@
-import { FaGithub, FaDiscord } from 'react-icons/fa';
-import { motion } from 'motion/react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGlobe } from 'react-icons/fa6';
+
+import { WaveDivider, SocialIcon } from '@/components/Common';
+import FooterRoutes from '@/components/Navigation/FooterRoutes';
+
+import ThemeLabLogo from '@/assets/themelab.svg';
+
+import { useThemeStore } from '@/store/theme';
 import { URLS } from '@/constants';
+
+export interface FooterMenu {
+    title: string,
+    url: string
+};
+
+const FOOTER_MENU: FooterMenu[] = [
+    {
+        title: 'Privacy Policy',
+        url: '/policies/privacy-policy'
+    },
+    {
+        title: 'Refund Policy',
+        url: '/policies/refund-policy'
+    },
+    {
+        title: 'Shipping Policy',
+        url: '/policies/shipping-policy'
+    },
+    {
+        title: 'Terms of Service',
+        url: '/policies/terms-of-service'
+    },
+];
 
 function Footer() {
 
+    const theme = useThemeStore((state) => state.currentTheme);
+
     return(
-        <div className="flex flex-col relative">
-            <div className="absolute bg-card top-0 h-5 w-full blur-md" />
-            <div className="flex bg-card justify-center py-5 px-4 md:px-50 z-20">
-                <div className="flex flex-1 z-10 items-center">
-                    <div className="flex-1 justify-start">
-                        <p className="text-sm">Copyright © {new Date().getFullYear()} ThemeLab</p>
+        <div className="relative z-20">
+            <footer className="bg-card-light py-10 flex flex-col gap-10 items-center justify-center">
+                <WaveDivider className="absolute z-40 w-full -top-10" bgColor={theme.colors.background} />
+                <div className="flex flex-col md:flex-row items-center justify-center md:w-6/12 gap-20 md:gap-0 pb-40">
+                    <div className="flex-1 flex justify-center md:justify-start">
+                        <img className="!relative !w-5/12" src={ThemeLabLogo} alt="theme lab logo" />
                     </div>
-                    <div className="flex-1 flex gap-3 text-2xl justify-end">
-                        <motion.div
-                            className="hover:cursor-pointer"
-                            whileHover={{ y: "-.5vh" }}
-                        >
-                            <a href={"https://discord.com"} target="_blank" rel="noopener noreferrer">
-                                <FaDiscord />
-                            </a>
-                        </motion.div>
-                        <motion.div
-                            className="hover:cursor-pointer"
-                            whileHover={{ y: "-.5vh" }}
-                        >
-                            <a href={URLS.GITHUB_REPO} target="_blank" rel="noopener noreferrer">
-                                <FaGithub />
-                            </a>
-                        </motion.div>
+                    <div className="flex flex-1 flex-col gap-6 items-center md:items-end -mt-1">
+                        <h1 className="font-obo font-semibold text-xl text-center md:text-left">Follow Me</h1>
+                        <div className="flex justify-center md:justify-end gap-6 w-full">
+                            <SocialIcon icon={FaLinkedin} tooltip="LinkedIn" to={URLS.LINKEDIN} size="3xl" />
+                            <SocialIcon icon={FaGlobe} tooltip="Personal Site" to={URLS.PERSONAL_SITE} size="3xl" />
+                            <SocialIcon icon={FaGithub} tooltip="GitHub" to={URLS.GITHUB_REPO} size="3xl" />
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="flex flex-col md:flex-row gap-10 text-xs font-semibold">
+                    <p className="order-2 md:order-1 text-center md:text-left">Copyright © {new Date().getFullYear()} Theme Lab</p>
+                    <div className="order-1 md:order-2">
+                        <FooterRoutes menu={FOOTER_MENU} />
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
